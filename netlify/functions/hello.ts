36f7988ad869168b7310825cc051893a449d8626
purboindra <purboindra@gmail.com>
2026-04-14 19:02:38 +0700

    fix: any type handler hello.ts file

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,4 +1,4 @@
-export const handler = async (event) => {
+export const handler = async (event: any) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from a minimal Netlify function!" }),
